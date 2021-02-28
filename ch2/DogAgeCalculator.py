87ef82c8dc9b05f1e2aef8e6d88d41249337b75f
tomvdmade <tomvdmade@gmail.com>
2021-02-28 19:09:29 +0100

    hfltc

---
+++ ch2/DogAgeCalculator.py
@@ -0,0 +1,8 @@
+dog_name = input("What is your dog's name? ")
+
+dog_age = input("what is your dog's age? ")
+
+dog_human_age = int(dog_age) * 7
+
+print(f"Your dog {dog_name} is {dog_human_age} years old in human years.")
+
---
+++ ch2/temp.py
@@ -0,0 +1,9 @@
+first = 'somewhere'
+last = 'over the rainbow'
+print(first, last)
+
+temp = first
+first = last
+last = temp
+
+print(first, last)
