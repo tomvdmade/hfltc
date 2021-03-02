f77bf5de6e08a1f64893c7b0cbfd33253aa5e79d
tomvdmade <tomvdmade@gmail.com>
2021-03-02 16:04:14 +0100

    hfltc

--- ch2/temp.py
+++ ch2/temp.py
@@ -2,8 +2,11 @@ first = 'somewhere'
 last = 'over the rainbow'
 print(first, last)
 
-temp = first
-first = last
-last = temp
+# temp = first
+# first = last
+# last = temp
+
+# new fast way > a, b = b, a
+first, last = last, first
 
 print(first, last)
---
+++ ch3/Boolean.py
@@ -0,0 +1,14 @@
+# print(3 > 5) is False (greater than)
+# print(5 >= 5) is True (greater than or equal to)
+# print(7 > 5) is True
+
+# print("pink" == "orange") is False  (equal to)
+# print("orange" == "orange") is True
+# print("pink" != "orange") is True (not equal to)
+
+if 10 >= 12:
+    print('Why not?')
+    print('Go ahead, buy it')
+else:
+    print('Sorry')
+    print('Try again next week')
